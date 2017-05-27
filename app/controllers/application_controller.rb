408d6384899b470a0684bc6ac2c5b1fe392911fb
Randy Russell <ransolo@me.com>
2017-05-27 01:07:21 -0500

    remove-space

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :prepare_meta_tags, if: "request.get?"
   def prepare_meta_tags(options={})
     site_name   = "EastKin"
-    title       = [controller_name, action_name].join(" ")
+    title       = [controller_name, action_name].join("")
     description = "A rock band from Nashville with a
         website that is informative and fun.  Detroy the universe of bring it new
         life. Good guy or bad guy"
