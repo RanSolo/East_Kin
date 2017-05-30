d5f3f4281c373edd5a37d7c509e084f1980953db
Randy Russell <ransolo@me.com>
2017-05-30 09:51:02 -0500

    after-not-before

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :set_welcome
-  before_action :prepare_meta_tags, if: "request.get?"
+  after_action :prepare_meta_tags, if: "request.get?"
 
   def prepare_meta_tags(options={})
     site_name   = "EastKin"
