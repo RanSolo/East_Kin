172e0f9575a71d527a3b6dcc640c7a120e85cfbc
Randy Russell <ransolo@me.com>
2017-05-30 10:14:40 -0500

    set-meta-tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,41 @@ class ApplicationController < ActionController::Base
   after_action :prepare_meta_tags, if: "request.get?"
 
   def prepare_meta_tags(options={})
-    
+    site_name   = "EastKin"
+    title       = [controller_name, action_name].join("")
+    description = "A rock band from Nashville with a
+        website that is informative and fun.  Detroy the universe of bring it new
+        life. Good guy or bad guy"
+    image       = options[:image] || "images/want_to_image"
+    current_url = request.url
+
+    # Prepare set of defaults
+    defaults = {
+      site:        site_name,
+      title:       title,
+      image:       image,
+      description: description,
+      keywords:    %w[rock, nashville, EastKin],
+      twitter: {
+        site_name: site_name,
+        site: '@eastkin',
+        card: 'summary',
+        description: description,
+        image: image
+      },
+      og: {
+        url: current_url,
+        site_name: site_name,
+        title: title,
+        image: image,
+        description: description,
+        type: 'video.movie'
+      }
+    }
+
+    options.reverse_merge!(defaults)
+
+    set_meta_tags options
   end
   def set_welcome
     @welcome = Welcome.find(1)
