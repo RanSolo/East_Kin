5401edf8b6bbf5babc6609d00c1a3b5f832c5478
Randy Russell <ransolo@me.com>
2018-04-11 18:59:42 -0500

    tests-up-to-adding-dependancies

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,6 +2,10 @@ module JobsHelper
   def sort_jobs_by_dependancies(jobs)
     if jobs.empty?
       'empty'
+    elsif jobs.count == 1
+      jobs[0].name
+    else
+      jobs.map(&:name).join ', '
     end
   end
 
--- test/helpers/jobs_helper_test.rb
+++ test/helpers/jobs_helper_test.rb
@@ -6,10 +6,24 @@ require 'test_helper'
 # Test for helper methods for Jobs
 #
 class JobsHelperTest < ActionView::TestCase
+  setup do
+    @jobs = Job.all
+  end
 
   test 'should return empty' do
-      @jobs = []
+    jobs = []
+    assert_match 'empty', sort_jobs_by_dependancies(jobs)
+  end
 
-      assert_match 'empty', sort_jobs_by_dependancies(@jobs)
+  test 'The result should be a sequence consisting of a single job a' do
+      assert_match 'a', sort_jobs_by_dependancies(@jobs)
+  end
+
+  test 'result should be a sequence containing all three jobs abc in no
+        significant order' do
+      job_b = Job.create(name: 'b')
+      job_c = Job.create(name: 'c')
+      @jobs = Job.all
+      assert_match 'a, b, c', sort_jobs_by_dependancies(@jobs)
   end
 end
