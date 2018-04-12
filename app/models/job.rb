890ff31c875db55e155c2ab7588d954b734b11c5
Randy Russell <ransolo@me.com>
2018-04-12 17:06:42 -0500

    add-mode-back-not-sure-what-happened-there

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,41 @@
 class Job < ApplicationRecord
+  validate :can_not_dep_on_self, on: :update
+  validate :jobs_cant_have_circular_dependencies, on: :update
+
+  def can_not_dep_on_self
+    unless id != dependant
+      errors.add(:dependant, "Jobs can’t depend on themselves")
+    end
+  end
+
+  def jobs_cant_have_circular_dependencies
+    jwds = find_jobs_with_dependancies(Job.all)
+    coll = []
+    jwds.each do |j|
+      collection = []
+      unless j.dependant.nil?
+        collection << [j.id, j.dependant]
+        dj = Job.find(j.dependant)
+      end
+      unless dj.dependant.nil?
+        collection << [dj.id, dj.dependant]
+
+      end
+      coll << collection
+      if coll[0][0][0] == self.dependant
+        return errors.add(:dependant, "No circular dependancies")
+      end
+    end
+  end
+
+  def find_jobs_with_dependancies(jobs)
+    collection = []
+    jobs.each do |job|
+      if job.dependant.present?
+        collection.push(job)
+      end
+    end
+    return collection
+  end
+
 end
