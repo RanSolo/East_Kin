745204f49f77f2bc51a461e4a6acf8ffbd5d91b4
Randy Russell <ransolo@me.com>
2018-04-12 17:05:58 -0500

    revert-a-few-things

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,29 +1,25 @@
 class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
+  before_action :set_jobs, only: [:index, :show, :new, :edit, :update, :destroy]
   layout "jobs"
   # GET /jobs
   # GET /jobs.json
   def index
     @jobs = Job.all
     @jobs_with_dependancies = find_jobs_with_dependancies(@jobs)
-
   end
 
   # GET /jobs/1
   # GET /jobs/1.json
-  def show
-  end
+  def show; end
 
   # GET /jobs/new
   def new
     @job = Job.new
-    @jobs = Job.all
   end
 
   # GET /jobs/1/edit
-  def edit
-    @jobs = Job.all
-  end
+  def edit; end
 
   # POST /jobs
   # POST /jobs.json
@@ -31,7 +27,7 @@ class JobsController < ApplicationController
     @job = Job.new(job_params)
     respond_to do |format|
       if @job.save
-        format.html { redirect_to @job, notice: 'Job was successfully created.' }
+        format.html { redirect_to @job, notice: 'Successfully created.' }
         format.json { render :show, status: :created, location: @job }
       else
         format.html { render :new }
@@ -43,9 +39,10 @@ class JobsController < ApplicationController
   # PATCH/PUT /jobs/1
   # PATCH/PUT /jobs/1.json
   def update
+    @jobs = Job.all
     respond_to do |format|
       if @job.update(job_params)
-        format.html { redirect_to @job, notice: 'Job was successfully updated.' }
+        format.html { redirect_to @job, notice: 'Successfully updated.' }
         format.json { render :show, status: :ok, location: @job }
       else
         format.html { render :edit }
@@ -59,19 +56,36 @@ class JobsController < ApplicationController
   def destroy
     @job.destroy
     respond_to do |format|
-      format.html { redirect_to jobs_url, notice: 'Job was successfully destroyed.' }
+      format.html { redirect_to jobs_url,
+                    notice: 'Job was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_job
-      @job = Job.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def job_params
-      params.require(:job).permit(:status, :dependant, :name, :label)
+  def find_jobs_with_dependancies(jobs)
+    collection = []
+    jobs.each do |job|
+      if job.dependant.present?
+        collection.push(job)
+      end
     end
+    return collection
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_job
+    @job = Job.find(params[:id])
+  end
+
+  def set_jobs
+    @jobs = Job.all
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list
+  # through.
+  def job_params
+    params.require(:job).permit(:status, :dependant, :name, :label)
+  end
 end
