01b52ef5c7c7ec05971760682b76fc8f4f52733c
Randy Russell <ransolo@me.com>
2018-04-12 19:36:34 -0500

    update-tests

--- test/models/job_test.rb
+++ test/models/job_test.rb
@@ -8,10 +8,9 @@ class JobTest < ActiveSupport::TestCase
   end
 
   test 'no circular dependancies.' do
-    job_d = Job.new(name: 'd')
-    job_c =Job.create(name: 'c', dependant: job_d.id)
-    job_b = Job.create(name: 'b', dependant: job_c.id)
-    job_d.update(dependant: job_b.id)
-    refute job_d.save
+    job_3 = Job.create(name: 'c')
+    job_2 = Job.create(name: 'b', dependant: Job.where(name: 'c')[0].id)
+    job_1 = Job.create(name: 'a', dependant: Job.where(name: 'b')[0].id)
+    refute job_3.update(dependant: job_1.id)
   end
 end
