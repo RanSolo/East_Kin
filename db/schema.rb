9fd6335578268b8552eee13de030d4e7f0321472
ransolo <ransolo@me.com>
2023-02-27 13:44:54 -0600

    sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -2,46 +2,45 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180411212045) do
-
+ActiveRecord::Schema[7.0].define(version: 2018_04_11_212045) do
   create_table "jobs", force: :cascade do |t|
-    t.boolean  "status"
-    t.integer  "dependant"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "label"
-    t.string   "name"
+    t.boolean "status"
+    t.integer "dependent"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.string "label"
+    t.string "name"
   end
 
   create_table "songs", force: :cascade do |t|
-    t.text    "lyric"
-    t.string  "title"
-    t.string  "writers"
-    t.text    "copyright"
+    t.text "lyric"
+    t.string "title"
+    t.string "writers"
+    t.text "copyright"
     t.boolean "active"
-    t.string  "youtube"
-    t.string  "facebook"
-    t.string  "soundcloud"
+    t.string "youtube"
+    t.string "facebook"
+    t.string "soundcloud"
   end
 
   create_table "welcomes", force: :cascade do |t|
-    t.string   "bandName"
-    t.string   "headline"
-    t.string   "message"
-    t.string   "logo"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.integer  "destroy_count"
-    t.integer  "restart_count"
-    t.boolean  "deathOrLife"
+    t.string "bandName"
+    t.string "headline"
+    t.string "message"
+    t.string "logo"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+    t.integer "destroy_count"
+    t.integer "restart_count"
+    t.boolean "deathOrLife"
   end
 
 end
